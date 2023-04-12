7b035688476ae55074b6d47ef8bcf27844243478
Gunjan <gunjan1800.cse18@chitkara.edu.in>
2023-04-12 10:49:49 +0530

    Adding all the Files

---
+++ src/main/java/com/example/demo/service/impl/EmployeeMapperImpl.java
@@ -0,0 +1,34 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.dto.Bonus;
+import com.example.demo.dto.Employee;
+import com.example.demo.service.EmployeeMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+
+public class EmployeeMapperImpl implements EmployeeMapper {
+
+
+    @Override
+    public Map<String, List<Employee>> map(List<Bonus> bonusList) {
+        Map<String, List<Employee>> employeesByCurrency = new HashMap<>();
+
+        bonusList.forEach(bonus -> {
+            List<Employee> employeesForCurrency = employeesByCurrency.computeIfAbsent(
+                    bonus.getCurrency(), k -> new ArrayList<>());
+
+            Employee employee = new Employee();
+            employee.setEmpName(bonus.getEmpName());
+            employee.setAmount(bonus.getAmount());
+            employeesForCurrency.add(employee);
+        });
+
+        employeesByCurrency.forEach((currency, employees) -> employees.sort(
+                Comparator.comparing(Employee::getEmpName)));
+
+        return employeesByCurrency;
+    }
+}
